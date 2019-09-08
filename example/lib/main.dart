61a1f89c21e55f37960340f7084a1537f308f8f0
Justin <justin.vietz@gmx.de>
2019-09-08 16:40:03 +0200

    simplyfied centering

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -34,20 +34,16 @@ class Example extends StatelessWidget {
         title: Text("Simple Example"),
       ),
       body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            NumberSlideAnimation(
-              number: "12345678987654321",
-              duration: const Duration(seconds: 2),
-              curve: Curves.decelerate,
-              textStyle: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold
-              ),
+        child: Center(
+          child: NumberSlideAnimation(
+            number: "12345678987654321",
+            duration: const Duration(seconds: 2),
+            curve: Curves.decelerate,
+            textStyle: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold
             ),
-          ],
+          ),
         )
       ),
     );
